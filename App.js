a22eb23051441c8208f516f10581bbf3a0f297cd
Sagar <sagrdagr22@gmail.com>
2022-10-07 16:52:12 +0530

    update App.js file

--- App.js
+++ App.js
@@ -1,33 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
-import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import Home from './screen/Home';
 
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <ImageBackground
-      imageStyle={styles.image}
-      style={styles.root}
-      source={
-        // uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUkZBcBSkjGkk_L64s_ki8F23wVRlEwU7zMQ&usqp=CAU',
-        require('./assets/images/red.jpg')
-      }
-      resizeMode="repeat">
-      <Text>OPR</Text>
-    </ImageBackground>
-  );
+   <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={Home} />
+    </Stack.Navigator>
+   </NavigationContainer>
+  )
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-  },
-  image: {
-    flex: 1,
-    // width: '100%',
-    // height:'100%',
-    opacity: 0.75,
-  },
-});
