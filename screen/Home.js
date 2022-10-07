bde42e422a93fa06f12694fcfa129e7808b6d65d
Sagar <sagrdagr22@gmail.com>
2022-10-07 17:31:18 +0530

    modify Home screen

--- screen/Home.js
+++ screen/Home.js
@@ -8,7 +8,7 @@ function Home() {
   style={styles.root}
   source={
     // uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUkZBcBSkjGkk_L64s_ki8F23wVRlEwU7zMQ&usqp=CAU',
-    require('./assets/images/red.jpg')
+    require('../assets/images/red.jpg')
   }
   resizeMode="repeat">
     <Text>Home Screen</Text>
