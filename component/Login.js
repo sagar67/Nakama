adb5e52f9d6a2dad3ffd3bb2c0d93ce92658596c
Sagar <sagrdagr22@gmail.com>
2022-10-07 18:19:18 +0530

    modify background image

--- component/Login.js
+++ component/Login.js
@@ -3,9 +3,13 @@ import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 function Login() {
   return (
-    <View>
+    <View style={styles.crewContainer}>
       <Text style={styles.label}>Crew</Text>
-      <TextInput placeholder="Crew Name" />
+      <TextInput
+        style={styles.input}
+        placeholder="Crew Name"
+        placeholderTextColor="white"
+      />
     </View>
   );
 }
@@ -13,11 +17,23 @@ function Login() {
 export default Login;
 
 const styles = StyleSheet.create({
+  crewContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   label: {
     textAlign: 'center',
     color: 'white',
-    fontSize: 18,
+    fontSize: 30,
     fontWeight: 'bold',
     // backgroundColor: 'white',
+  },
+  input: {
+    padding: 8,
+    borderWidth: 2,
+    borderColor: 'white',
+    borderRadius: 6,
+    margin: 10,
+    color: 'white',
   },
 });
--- screen/Home.js
+++ screen/Home.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import Login from '../component/Login';
 
 function Home() {
   return <View style={styles.root}>
     <ImageBackground
   imageStyle={styles.image}
+  blurRadius={1}
   style={styles.rootContainer}
   source={
     // uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUkZBcBSkjGkk_L64s_ki8F23wVRlEwU7zMQ&usqp=CAU',
     require('../assets/images/red.jpg')
   }
-  resizeMode="repeat">
-    <Text>Home Screen</Text>
+  resizeMode="cover">
+    <Login/>
 </ImageBackground>
   </View>
 }
@@ -29,6 +31,5 @@ const styles = StyleSheet.create({
       // flex: 1,
       // width: '100%',
       // height:'100%',
-      opacity: 0.75,
     },
   });
